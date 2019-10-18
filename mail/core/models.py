0326d9910b2a51a7c1e7286b8f3b92c93c8a66a8
Johnatas <johnatasr@hotmail.com>
2019-10-18 00:55:35 -0300

    #1 - Refazendo

---
+++ mail/core/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class ConfirmationToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="confirmation_token")
+    confirmation_key = models.CharField(max_length=100)
+    is_confirmed = models.BooleanField(default=False)
